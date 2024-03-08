d74a5d8e8fea06f7e4500ea979757e5459514980
dyadyasawa <dyadyasawa@gmail.com>
2024-03-08 15:53:19 +0300

    Создание шаблонов home и contact.

--- catalog/views.py
+++ catalog/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def catalog(request):
-    return render(request, 'catalog_app/index.html')
+    #return render(request, 'catalog_app/index.html')
+    return render(request, 'catalog_app/home.html')
