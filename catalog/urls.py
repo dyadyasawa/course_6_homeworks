7bf19a77476a850d38f8d37277c3cd52fece0e9b
dyadyasawa <dyadyasawa@gmail.com>
2024-03-11 13:03:38 +0300

    Правки в catalog/apps.py и catalog/urls.py.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
-
 from catalog.views import home, contact
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contact)
+    path('', home, name="home"),
+    path('contacts/', contact, name="contact")
 ]
