2bde416c6367e41a6a198b2a9c04511bae4e1ee6
dyadyasawa <dyadyasawa@gmail.com>
2024-03-11 16:20:05 +0300

    Правки в settings.py.

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_course_6',
+        'USER': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
+        'PASSWORD': 'paragWay_38' # Вынести в переменную окружения
     }
 }
 
