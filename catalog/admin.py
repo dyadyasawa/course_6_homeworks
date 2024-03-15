f3f98dbc96a2641d50125b321d08e60fd75193c2
dyadyasawa <dyadyasawa@gmail.com>
2024-03-15 15:38:49 +0300

    Изменения в admin.py. Это контрольный коммит перед внесением изменений в модель Product.

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 
 from catalog.models import Product, Category
 
-#admin.site.register(Product)
+admin.site.register(Product)
 admin.site.register(Category)
 
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'category',)
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'price', 'category',)
 
 
 
