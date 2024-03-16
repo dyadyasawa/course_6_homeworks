dd01e8ea769ff17282331f06517c621be42cf19d
dyadyasawa <dyadyasawa@gmail.com>
2024-03-16 08:09:34 +0300

    Изменения в catalog/models.py.

--- catalog/models.py
+++ catalog/models.py
@@ -1,21 +1,22 @@
-from datetime import date
 
 from django.db import models
 
 
-NULLABLE = {'blank': True, 'null': True }
+NULLABLE = {'blank': True, 'null': True}
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='product/', verbose_name='Изображение', **NULLABLE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена за покупку')
-    created_at = models.DateTimeField(verbose_name='Дата создания') # установка на заполнение при создании
-    updated_at = models.DateTimeField(verbose_name='Дата изменения') # установка на заполнение при изменении
+
+    created_at = models.DateTimeField(auto_now_add=True, eitable=False, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name='Дата изменения')
 
     def __str__(self):
-        return f'{self.name}, {self.category}, {self.price}' #, {self.created_at}'
-
+        return f'{self.name}, {self.category}, {self.price}'
 
     class Meta:
         verbose_name = 'Товар'
@@ -29,7 +30,6 @@ class Category(models.Model):
 
     def __str__(self):
         return f'{self.name}'
-
 
     class Meta:
         verbose_name = 'Категория'
